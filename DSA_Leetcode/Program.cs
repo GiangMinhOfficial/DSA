b75261c18299ec4558d33ecf795af7bec3cb5896
GiangMinhOfficial <ghieuming@gmail.com>
2024-06-08 16:26:54 +0700

    leetcode problem: [easy] - 35

---
+++ DSA_Leetcode/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+
+using DSA_Leetcode;
+
+int[] nums = new int[] { 1, 3, 5, 6 };
+int target = 4;
+
+int res = Search_Insert_Position_35.SearchInsert(nums, target);
+Console.WriteLine("result: " + res);
---
+++ DSA_Leetcode/Search_Insert_Position_35.cs
@@ -0,0 +1,41 @@
+﻿namespace DSA_Leetcode
+{
+    internal class Search_Insert_Position_35
+    {
+        // https://leetcode.com/problems/search-insert-position/description/
+        //Given a sorted array of distinct integers and a target value, return the index if the target is found.If not, return the index where it would be if it were inserted in order.
+        //You must write an algorithm with O(log n) runtime complexity.
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            if (target < nums[0])
+                return 0;
+
+            if (target > nums[^1])
+                return nums.Length;
+
+            int oldId = -3;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int arrVal = nums[i];
+                if (target >= arrVal)
+                {
+                    if (target == arrVal)
+                    {
+                        return i;
+                    }
+
+                    oldId = i;
+                }
+                else
+                {
+                    oldId++;
+                    break;
+                }
+            }
+
+            return oldId;
+        }
+    }
+}
