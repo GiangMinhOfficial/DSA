9b8ea23899058dbae46eee644000a079b09396e2
GiangMinhOfficial <ghieuming@gmail.com>
2024-06-09 21:47:43 +0700

    58. Length of Last Word

---
+++ DSA_Leetcode/Length_of_Last_Word_58.cs
@@ -0,0 +1,38 @@
+﻿namespace DSA_Leetcode
+{
+    internal class Length_of_Last_Word_58
+    {
+        // https://leetcode.com/problems/length-of-last-word/
+
+        //Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+        //A word is a maximal substring consisting of non-space characters only.
+
+        public static int LengthOfLastWord(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+
+            int length = 0;
+            bool flag = false;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ')
+                {
+                    if (flag)
+                        break;
+
+                    continue;
+                }
+                else if (s[i] != ' ')
+                {
+                    flag = true;
+                    length++;
+                }
+
+            }
+
+            return length;
+        }
+    }
+}
--- DSA_Leetcode/Program.cs
+++ DSA_Leetcode/Program.cs
@@ -1,11 +1,16 @@
 ﻿
 
-
-
 using DSA_Leetcode;
 
-int[] nums = new int[] { 1, 3, 5, 6 };
-int target = 4;
+string s = "Hello World";
+int res = Length_of_Last_Word_58.LengthOfLastWord(s);
+Console.WriteLine(res);
 
-int res = Search_Insert_Position_35.SearchInsert(nums, target);
-Console.WriteLine("result: " + res);
+
+#region 35
+//int[] nums = new int[] { 1, 3, 5, 6 };
+//int target = 4;
+
+//int res = Search_Insert_Position_35.SearchInsert(nums, target);
+//Console.WriteLine("result: " + res);
+#endregion
